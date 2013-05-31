6fda5c3de2e95f2e4006ac02171a1f972697dc50
Michal Grzywna <michal@grzywna.me>
2013-05-31 23:33:05 +0200

    corrected navbar height

--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'slim'
+require 'sass'
+
+Slim::Engine.set_default_options pretty: true
+
+get('/css/style.css') { scss :'styles/style' }
 
 get '/' do
   slim :home

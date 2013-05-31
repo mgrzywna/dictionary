b2523f5a6d86a4db1c9889dd227b18321bbdc641
Michal Grzywna <michal@grzywna.me>
2013-06-01 01:32:16 +0200

    added search box

--- main.rb
+++ main.rb
@@ -3,10 +3,17 @@ require 'sinatra/reloader' if development?
 require 'slim'
 require 'sass'
 
+require './model'
+
 Slim::Engine.set_default_options pretty: true
 
 get('/css/style.css') { scss :'styles/style' }
 
 get '/' do
-  slim :home
+  slim :search_form
+end
+
+get '/search' do
+  @words = Word.all(:name.like => "#{params[:word]}")
+  slim :search
 end

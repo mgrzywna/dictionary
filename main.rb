c7c3252f08b8000dbf4591665ee2af5bea7d9e95
Michal Grzywna <michal@grzywna.me>
2013-06-01 02:52:07 +0200

    changed navigation

--- main.rb
+++ main.rb
@@ -7,6 +7,11 @@ require './model'
 
 Slim::Engine.set_default_options pretty: true
 
+$navigation = {
+  :add => 'ADD NEW WORD',
+  :editlang => 'EDIT LANGUAGES'
+}
+
 get('/css/style.css') { scss :'styles/style' }
 
 get '/' do
@@ -16,4 +21,27 @@ end
 get '/search' do
   @words = Word.all(:name.like => "#{params[:word]}")
   slim :search
+end
+
+get '/add' do
+  @languages = Language.all
+  @active_page = :add
+  slim :add
+end
+
+post '/add' do
+  redirect '/edit'
+end
+
+get '/edit' do
+  slim :edit
+end
+
+post '/edit' do
+  redirect '/edit'
+end
+
+get '/editlang' do
+  @active_page = :editlang
+  slim :editlang
 end

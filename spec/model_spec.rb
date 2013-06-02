2c4f32bba2babb6d19f09679151249c02328e04e
Michal Grzywna <michal@grzywna.me>
2013-06-02 23:37:08 +0200

    Update spec for Language

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -1,24 +1,25 @@
 require 'spec_helper'
 
 describe 'Language' do
-  before :each do
-    @language = Language.new(name: 'English')
-  end
-
   it 'is valid with valid attributes' do
+    Language.destroy
+    @language = Language.new name: 'English'
     @language.should be_valid
   end
 
   it 'is not valid without a name' do
+    Language.destroy
+    @language = Language.new name: nil
     @language.name = nil
     @language.should_not be_valid
   end
 
-  it 'is unique' do
-    en = Language.new(name: 'English')
+  it 'is not valid when name is not unique ' do
+    Language.destroy
+    en = Language.new name: 'English'
     en.save
     en.should be_valid
-    en = Language.new(name: 'English')
+    en = Language.new name: 'English'
     en.should_not be_valid
   end
 end

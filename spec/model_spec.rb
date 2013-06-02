bdc11908d36cf85410d5576c74561e4b8b53d0d1
Michal Grzywna <michal@grzywna.me>
2013-06-03 00:04:52 +0200

    Add spec for TranslationPair

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -27,7 +27,6 @@ end
 
 describe 'Word' do
   before do
-    Language.destroy
     @lang = Language.first_or_create name: 'English'
     Word.destroy
   end
@@ -53,5 +52,37 @@ describe 'Word' do
     word.should be_valid
     word = Word.new name: 'awesome', language: @lang
     word.should_not be_valid
+  end
+end
+
+describe 'TranslationPair' do
+  before do
+    lang = Language.first_or_create name: 'English'
+    @word_a = Word.first_or_create name: 'a', language: lang
+    @word_b = Word.first_or_create name: 'b', language: lang
+    TranslationPair.destroy
+  end
+
+  it 'is valid with valid attributes' do
+    tp = TranslationPair.new first: @word_a, second: @word_b
+    tp.should be_valid
+  end
+
+  it 'is not valid without first word' do
+    tp = TranslationPair.new second: @word_b
+    tp.should_not be_valid
+  end
+
+  it 'is not valid without second word' do
+    tp = TranslationPair.new first: @word_a
+    tp.should_not be_valid
+  end
+
+  it 'is not valid when pair is not unique' do
+    tp = TranslationPair.new first: @word_a, second: @word_b
+    tp.save
+    tp.should be_valid
+    tp = TranslationPair.new first: @word_a, second: @word_b
+    tp.should_not be_valid
   end
 end

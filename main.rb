daa401983b56db24d3fa422d38445ccb96b49e41
Michal Grzywna <michal@grzywna.me>
2013-06-02 22:58:27 +0200

    Add configuration depending on environment

--- main.rb
+++ main.rb
@@ -1,78 +1,118 @@
 # -*- encoding : utf-8 -*-
 
 require 'sinatra'
-require 'sinatra/reloader' if development?
 require 'slim'
 require 'sass'
 
 require './model'
 
-Slim::Engine.set_default_options pretty: true
+
+configure :production do
+  Slim::Engine.set_default_options :pretty => false
+end
+
+
+configure :test do
+  Slim::Engine.set_default_options :pretty => true
+end
+
+
+configure :development do
+  require 'sinatra/reloader'
+  Slim::Engine.set_default_options :pretty => true
+end
+
 
 get('/css/style.css') { scss :'styles/style' }
 not_found { slim :not_found }
+
 
 get '/' do
   slim :'components/search_form'
 end
 
+
 get '/search' do
   word = params[:word].split.join(' ')
-  if word.empty? then redirect '/' end
+
+  if word.empty?
+    redirect '/'
+  end
+
   words = Word.all :name.like => "%#{word}%", :limit => 30
   @words = words.select { |word| word.translations.count > 0 }
   slim :search
 end
+
 
 get '/word/:word_id' do
   @word = Word.get(params[:word_id]) or halt(404)
   slim :word
 end
 
+
 get '/add-word' do
   @languages = Language.all
+
   @form = {
     :action => '/add-word',
     :label => 'Add word',
     :input_name => 'word'
   }
+
   slim :'components/add_word_form'
 end
+
 
 post '/add-word' do
   language = Language.get params[:language]
   word = params[:word].split.join(' ')
-  if word.empty? then redirect '/add-word' end
+
+  if word.empty?
+    redirect '/add-word'
+  end
+
   word = Word.first_or_create :name => word, :language => language
   redirect "/add-translation/#{word.id}"
 end
 
+
 get '/add-translation/:word_id' do
-  @word = Word.get(params[:word_id]) or halt(404)
+  @word = Word.get(params[:word_id])  or halt(404)
   @languages = Language.all
+
   @form = {
     :action => "/add-translation/#{@word.id}",
     :label => 'Add translation',
     :input_name => 'translation'
   }
+
   slim :add_translation
 end
+
 
 post '/add-translation/:word_id' do
   word = Word.get params[:word_id]
   language = Language.get params[:language]
   translation = params[:translation].split.join(' ')
-  if translation.empty? then redirect "/add-translation/#{word.id}" end
+
+  if translation.empty?
+    redirect "/add-translation/#{word.id}"
+  end
+
   translation = Word.first_or_create :name => translation, :language => language
   TranslationPair.create :first => word, :second => translation
   TranslationPair.create :first => translation, :second => word
   redirect "/add-translation/#{word.id}"
 end
 
+
 post '/add-language' do
   language = params[:language].split.join(' ')
+
   unless language.empty?
     Language.first_or_create :name => params[:language]
   end
+
   redirect "#{params[:redirect]}"
 end
--- model.rb
+++ model.rb
@@ -2,45 +2,46 @@
 
 require 'data_mapper'
 
-DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/model.db")
-DataMapper::Model.raise_on_save_failure = true
+configure :production do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/production.db")
+  DataMapper::Model.raise_on_save_failure = false
+end
 
+configure :test do
+  DataMapper.setup(:default, 'sqlite::memory:')
+  DataMapper::Model.raise_on_save_failure = true
+end
+
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+  DataMapper::Model.raise_on_save_failure = true
+end
 
 class Language
   include DataMapper::Resource
-
   property :id, Serial
   property :name, String
-
   validates_presence_of :name
   validates_uniqueness_of :name
-
   has n, :words
 end
 
-
 class Word
   include DataMapper::Resource
-
   property :id, Serial
   property :name, String
-
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :language
-
   belongs_to :language
   has n, :translation_pairs, :child_key => [ :first_id ]
   has n, :translations, self, :through => :translation_pairs, :via => :second
 end
 
-
 class TranslationPair
   include DataMapper::Resource
-
   belongs_to :first, 'Word', :key => true
   belongs_to :second, 'Word', :key => true
 end
-
 
 DataMapper.finalize
 DataMapper.auto_upgrade!

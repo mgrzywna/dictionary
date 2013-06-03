f34ad573d105f0380519000fec0067bde0a87dbd
Michal Grzywna <michal@grzywna.me>
2013-06-03 07:11:32 +0200

    Refactor specs for model

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -1,88 +1,51 @@
 require 'spec_helper'
 
 describe 'Language' do
-  before do
-    Language.destroy
+  it 'should require a name' do
+    Language.create(name: 'English').should be_valid
+    Language.new().should_not be_valid
   end
 
-  it 'is valid with valid attributes' do
-    language = Language.new name: 'English'
-    language.should be_valid
-  end
-
-  it 'is not valid without a name' do
-    language = Language.new
-    language.name = nil
-    language.should_not be_valid
-  end
-
-  it 'is not valid when name is not unique ' do
-    en = Language.new name: 'English'
-    en.save
-    en.should be_valid
-    en = Language.new name: 'English'
-    en.should_not be_valid
+  it 'should have unique name' do
+    Language.create(name: 'German').should be_valid
+    Language.new(name: 'German').should_not be_valid
   end
 end
 
 describe 'Word' do
-  before do
-    @lang = Language.first_or_create name: 'English'
-    Word.destroy
+  before :each do
+    @english = Language.first_or_create(name: 'English')
+    @german = Language.first_or_create(name: 'German')
   end
 
-  it 'is valid with valid attributes' do
-    word = Word.new name: 'ruby', language: @lang
-    word.should be_valid
+  it 'should require a name and language' do
+    Word.new(name: 'ruby', language: @english).should be_valid
+    Word.new(language: @english).should_not be_valid
+    Word.new(name: 'python').should_not be_valid
   end
 
-  it 'is not valid without a name' do
-    word = Word.new language: @lang
-    word.should_not be_valid
-  end
-
-  it 'is not valid without a language' do
-    word = Word.new name: 'python'
-    word.should_not be_valid
-  end
-
-  it 'is not valid when name is not unique within scope of one language' do
-    word = Word.new name: 'awesome', language: @lang
-    word.save
-    word.should be_valid
-    word = Word.new name: 'awesome', language: @lang
-    word.should_not be_valid
+  it 'should have unique name within scope of one language' do
+    Word.create(name: 'taxi', language: @english).should be_valid
+    Word.new(name: 'taxi', language: @english).should_not be_valid
+    Word.new(name: 'taxi', language: @german).should be_valid
   end
 end
 
 describe 'TranslationPair' do
   before do
-    lang = Language.first_or_create name: 'English'
-    @word_a = Word.first_or_create name: 'a', language: lang
-    @word_b = Word.first_or_create name: 'b', language: lang
-    TranslationPair.destroy
+    english = Language.first_or_create(name: 'English')
+    @foo = Word.first_or_create(name: 'foo', language: english)
+    @bar = Word.first_or_create(name: 'bar', language: english)
   end
 
-  it 'is valid with valid attributes' do
-    tp = TranslationPair.new first: @word_a, second: @word_b
-    tp.should be_valid
+  it 'should require both words' do
+    TranslationPair.new(first: @foo, second: @bar).should be_valid
+    TranslationPair.new(first: @foo).should_not be_valid
+    TranslationPair.new(second: @bar).should_not be_valid
   end
 
-  it 'is not valid without first word' do
-    tp = TranslationPair.new second: @word_b
-    tp.should_not be_valid
-  end
-
-  it 'is not valid without second word' do
-    tp = TranslationPair.new first: @word_a
-    tp.should_not be_valid
-  end
-
-  it 'is not valid when pair is not unique' do
-    tp = TranslationPair.new first: @word_a, second: @word_b
-    tp.save
-    tp.should be_valid
-    tp = TranslationPair.new first: @word_a, second: @word_b
-    tp.should_not be_valid
+  it 'should be unique' do
+    TranslationPair.create(first: @foo, second: @bar).should be_valid
+    TranslationPair.new(first: @foo, second: @bar).should_not be_valid
   end
 end

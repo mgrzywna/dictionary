33886c832f2659a2dee3652df19ba8a0e77f205c
Michal Grzywna <michal@grzywna.me>
2013-06-03 10:02:56 +0200

    Add more specs

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -10,6 +10,14 @@ describe "Dictionary" do
     @polish = Language.first_or_create(name: "Polish")
   end
 
+  describe "not found" do
+    it "should give 404" do
+      get "/something"
+      last_response.status.should == 404
+      last_response.body.should match /Go to the homepage/
+    end
+  end
+
   describe "get /" do
     it "should load the home page" do
       get "/"
@@ -52,11 +60,52 @@ describe "Dictionary" do
     end
   end
 
-  describe "not found" do
-    it "should give 404" do
-      get "/something"
+  describe "get /word/:word_id" do
+    it "should show word" do
+      foo = en = Word.first_or_create(name: "foo", language: @english)
+      bar = en = Word.first_or_create(name: "bar", language: @english)
+
+      get "/word/#{foo.id}"
+      last_response.should be_ok
+      last_response.body.should match /foo/
+
+      get "/word/#{bar.id}"
+      last_response.should be_ok
+      last_response.body.should match /bar/
+    end
+
+    it "should give 404 when word cannot be found" do
+      get "/word/123"
       last_response.status.should == 404
-      last_response.body.should match /Go to the homepage/
     end
   end
+
+  describe "get /add-word" do
+    it "should show form" do
+      get "/add-word"
+      last_response.should be_ok
+      last_response.should match /<form/
+    end
+  end
+
+  describe "post /add-word" do
+    it "should create a new word" do
+      post "/add-word", language: @english.id, word: "guitar"
+      word = Word.first
+      word.name.should == "guitar"
+
+      follow_redirect!
+      last_request.url.should == "http://example.org/add-translation/#{word.id}"
+      last_response.should be_ok
+    end
+
+    it "should redirect to /add-word when given empty word" do
+      post "/add-word", language: @english.id, word: ""
+
+      follow_redirect!
+      last_request.url.should == "http://example.org/add-word"
+      last_response.should be_ok
+    end
+  end
+
 end
